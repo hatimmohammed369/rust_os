a7bdd889ce2ccaf936b46291e1b4c42abc588375
Hatim Muhammed <hatimmohammed369@gmail.com>
2022-03-19 06:38:21 +0200

    Fixed eh_personality/panic_handler issue

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 #![no_std] // NO STANDARD LIBRARY
+
+use core::panic::PanicInfo;
+
+/// This function is called on panic, it's the new panic handler
+/// the default was provided by the standard library
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
 fn main() {
 }
